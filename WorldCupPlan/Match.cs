50a87d02af86d1a87eef6ca1f40a0e04958bbdd0
gusbrin <gus.villegasb@gmail.com>
2017-08-22 23:48:09 -0700

    First Checkin of the Project

---
+++ WorldCupPlan/Match.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorldCupPlan
+{
+    class Match
+    {
+        public string Local { get; set; }
+        public string Visitor { get; set; }
+        public Venue Venue { get; set; }
+        public DateTime MatchDateTimeUTC { get; set; }
+        public string MatchName { get; set; }
+
+    }
+}
